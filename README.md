caecf01952fa74ce04aaf8a92383efcf29c2bd16
Carl Alexander <carlalexander@gmail.com>
2022-03-10 12:24:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+<p align="center">
+    <a href="https://ymirapp.com" target="_blank" align="center">
+        <img src="https://cdn-std.droplr.net/files/acc_680806/69fc3k" width="280">
+    </a>
+</p>
+
+# Ymir image processing function
+
+[Ymir][1] [Lambda@Edge][3] function used by [CloudFront][4] to process and optimize images.
+
+## Installation
+
+Install the depencies using npm:
+
+```
+$ npm ci
+```
+
+## Building
+
+To build a zip archive of the Lambda function to upload to AWS, you can run the following command:
+
+```
+$ npm run build-zip
+```
+
+## Links
+
+ * [Documentation][2]
+
+[1]: https://ymirapp.com
+[2]: https://docs.ymirapp.com
+[3]: https://aws.amazon.com/lambda/edge/
+[4]: https://aws.amazon.com/cloudfront/
---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const animated = require('animated-gif-detector'),
+      aws = require('aws-sdk'),
+      sharp = require('sharp'),
+      s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+exports.handler = async (event, context, callback) => {
+    try {
+        const request = event.Records[0].cf.request;
+        let response = event.Records[0].cf.response;
+
+        if ('200' !== response.status) {
+            return callback(null, response);
+        }
+
+        const bucket = request.origin.s3.domainName.match(/([^.]*)\.s3\.amazonaws\.com/i)[1];
+
+        if ('string' !== typeof bucket) {
+            return callback(null, response);
+        }
+
+        const allowedContentTypes = ['image/gif', 'image/jpeg', 'image/png'];
+        const key = decodeURIComponent(request.uri.substring(1));
+        const object = await s3.getObject({ Bucket: bucket, Key: key }).promise();
+        
+        if (!object.ContentType 
+          || !allowedContentTypes.includes(object.ContentType)
+          || ('image/gif' === object.ContentType && animated(object.Body))
+        ) {
+            return callback(null, response);
+        }
+        
+        let contentType = null;
+        const image = sharp(object.Body);
+        const params = new URLSearchParams(request.querystring);
+
+        if ('image/gif' === object.ContentType) {
+            image.png();
+            contentType = [{ value: 'image/png' }];
+        }
+
+        if (request.headers['accept'] && request.headers['accept'][0].value.match('image/webp')) {
+            image.webp({ quality: Math.round(Math.min(Math.max(parseInt(params.get('quality'), 10) || 82, 0), 100)) });
+            contentType = [{ key: 'Content-Type', value: 'image/webp' }];
+        }
+
+        if (params.has('width') || params.has('height')) {
+            image.resize({
+                width: parseInt(params.get('width'), 10) || null,
+                height: parseInt(params.get('height'), 10) || null,
+                fit: params.has('cropped') ? sharp.fit.cover : sharp.fit.inside,
+                withoutEnlargement: true,
+            });
+        }
+
+        const buffer = await image.toBuffer();
+        const responseBody = buffer.toString('base64');
+
+        if (1330000 < Buffer.byteLength(responseBody)) {
+            return callback(null, response);
+        }
+
+        if (contentType) {
+            response.headers['content-type'] = contentType;
+        }
+
+        response.body = responseBody;
+        response.bodyEncoding = 'base64';
+
+        callback(null, response);
+    } catch (error) {
+        console.log(error);
+    }
+};
